ac9670df7b9e217600a4c00859867f1f02601b95
Ali Hamza Kamboh <alihamzakamboh180@gmail.com>
2024-08-18 19:51:38 +0500

    upadted

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,7 +13,7 @@ export const Hero = () => {
   return (
     <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip">
       <div className="container">
-        <div className='md:flex items-center mt-0 lg:mt-12'>
+        <div className='md:flex items-center lg:mt-0 mt-12'>
           <div className='md:w-[478px]'>
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Under Dev</div>
             <h1 className="text-5xl md:7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Spidex: Web Scraper</h1>
